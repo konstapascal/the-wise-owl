730edb7dc421f1ee66421da738d55bac0d63e9fb
konstapascal <pascalkonstantinos@gmail.com>
2021-06-05 19:35:53 +0200

    small fixes to links

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,12 +7,12 @@ function Navbar() {
 				<p className='text-4xl italic font-bold text-yellow-700'>The Wise Owl</p>
 			</div>
 			<ul className=' flex items-center gap-4 text-2xl text-yellow-700'>
-				<Link href={`/the-wise-owl`}>
+				<Link href={`/`}>
 					<a className=' hover:bg-yellow-400 active:scale-95 px-4 py-2 text-xl font-semibold transform bg-yellow-300 cursor-pointer'>
 						Generate Quotes
 					</a>
 				</Link>
-				<Link href={`/the-wise-owl/quotes`}>
+				<Link href={`/quotes`}>
 					<a className=' hover:bg-yellow-400 active:scale-95 px-4 py-2 text-xl font-semibold transform bg-yellow-300 cursor-pointer'>
 						My Quotes
 					</a>

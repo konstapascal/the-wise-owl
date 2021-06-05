9536eba094d7caa3366cc9c7d096fcb2b5bb67e9
konstapascal <pascalkonstantinos@gmail.com>
2021-06-05 19:32:15 +0200

    fixed next.config, Images, hrefs

--- components/Quote.js
+++ components/Quote.js
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-
 function Quote({ id, content, author, deleteQuote }) {
 	return (
 		<div className='lg:flex-row flex flex-col justify-between my-8'>
@@ -7,10 +5,10 @@ function Quote({ id, content, author, deleteQuote }) {
 				<p className='max-w-2xl mb-2 text-xl'>{content}</p>
 				<p> {`- ${author}`}</p>
 			</div>
-			<Image
+			<img
 				onClick={() => deleteQuote(id)}
 				className='cursor-pointer'
-				src='/trash-solid.svg'
+				src='/the-wise-owl/trash-solid.svg'
 				width={20}
 				height={20}
 			/>
---
+++ next.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+	basePath: '/the-wise-owl'
+};
